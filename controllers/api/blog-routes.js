bc9c2ef05801b6efdab8c5a5d3f363dbbaf75462
jana <vs.janarthani@gmail.com>
2021-05-01 03:03:20 -0700

    added api routes for users and blogs

--- controllers/api/blog-routes.js
+++ controllers/api/blog-routes.js
@@ -3,45 +3,91 @@ const { Blog, User } = require('../../models');
 
 // GET /api/blogs
 router.get('/', async (req, res) => {
-    try {
-        const allBlogs = await Blog.findAll({include: [User], 
-            attributes: { exclude: ['password'] }  
-        });
-        res.status(200).json(allBlogs);
-      }
-      catch (e) {
-        res.status(400).json({Error: e});
-      }
+  try {
+    const allBlogs = await Blog.findAll({
+      include: [{
+        model: User,
+        attributes: ['username'] 
+      }]
+    });
+    res.status(200).json(allBlogs);
+  }
+  catch (e) {
+    res.status(400).json({ Error: e });
+  }
 });
 
-// GET /api/blogs/1
-router.get('/:id', async (req, res) => {   
-    try {
-        const { id } = req.params;
-        const blogById = await Blog.findOne({
-            where: { id }
-        });
-        res.status(200).json(blogById);
-      }
-      catch (e) {
-        res.status(400).json({Error: e});
-      }
+// GET /api/blogs/id
+router.get('/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const blogById = await Blog.findOne({
+      where: { id }
+    });
+    if (!blogById) {
+      return res.status(404).json({ message: 'No Blog found with this id' });
+    }
+    res.status(200).json(blogById);
+  }
+  catch (e) {
+    res.status(400).json({ Error: e });
+  }
 });
 
 // POST /api/blogs
 router.post('/', async (req, res) => {
-    try {
-        const { blog_title, blog_contents, user_id } = req.body
-        const newBlog =  await Blog.create({
-            blog_title, 
-            blog_contents,
-            user_id
-          });
-          res.status(200).json(newBlog);
+  try {
+    console.log(req.body);
+    const { blog_title, blog_contents, user_id } = req.body;
+    console.log("Post Route", blog_title, blog_contents, user_id);
+    const newBlog = await Blog.create({
+      blog_title,
+      blog_contents,
+      user_id
+    });
+    res.status(200).json(newBlog);
+  }
+  catch (e) {
+    res.status(400).json({ Error: e });
+  }
+});
+
+// PUT /api/blogs/id
+router.put('/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    await Blog.update(req.body, {
+      where: { id }
+    });
+    const blogById = await Blog.findOne({
+      where: { id }
+    });
+    if (!blogById) {
+      return res.status(404).json({ message: 'No Blog found with this id' });
     }
-    catch (e) {
-        res.status(400).json({Error: e});
-      }
+    res.status(200).json({Updated: blogById});
+  }
+  catch (e) {
+    res.status(400).json({ Error: e });
+  }
+});
+
+// DELETE /api/blogs/id
+router.delete('/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const blogById = await Blog.findOne({
+      where: { id }
+    });
+    if (!blogById) {
+      return res.status(404).json({ message: 'No blog found with this id' });
+    }
+    await Blog.destroy({ where: { id } });
+    res.status(200).json({ Deleted: blogById });
+
+  } catch (e) {
+    res.status(400).json(e);
+  }
 });
 
 module.exports = router;
--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -3,57 +3,91 @@ const { User } = require('../../models');
 
 // GET /api/users
 router.get('/', async (req, res) => {
-    try {
-        const allUsers = await User.findAll(({
-            attributes: { exclude: ['password'] }
-          }));
-        res.status(200).json(allUsers);
-      }
-      catch (e) {
-        res.status(400).json({Error: e});
-      }
+  try {
+    const allUsers = await User.findAll(({
+      attributes: { exclude: ['password'] }
+    }));
+    res.status(200).json(allUsers);
+  }
+  catch (e) {
+    res.status(400).json({ Error: e });
+  }
 });
 
-// GET /api/users/1
+// GET /api/users/id
 router.get('/:id', async (req, res) => {
-    
-    try {
-        const { id } = req.params;
-        const userById = await User.findOne({
-            attributes: { exclude: ['password'] },
-            where: { id }
-        });
-        res.status(200).json(userById);
-      }
-      catch (e) {
-        res.status(400).json({Error: e});
-      }
+
+  try {
+    const { id } = req.params;
+    const userById = await User.findOne({
+      attributes: { exclude: ['password'] },
+      where: { id }
+    });
+    if (!userById) {
+      return res.status(404).json({ message: 'No user found with this id' });
+    }
+    res.status(200).json(userById);
+  }
+  catch (e) {
+    res.status(400).json({ Error: e });
+  }
 });
 
 // POST /api/users
 router.post('/', async (req, res) => {
-    try {
-        const { username, email, password } = req.body
-        console.log(username, email, password);
-        const newUser =  await User.create({
-            username,
-            email,
-            password
-          });
-          res.status(200).json(newUser);
+  try {
+    const { username, email, password } = req.body
+    console.log(username, email, password);
+    const newUser = await User.create({
+      username,
+      email,
+      password
+    });
+    res.status(200).json(newUser);
+  }
+  catch (e) {
+    res.status(400).json({ Error: e });
+  }
+});
+
+// PUT /api/users/id
+router.put('/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    await User.update(req.body, {
+      where: { id }
+    });
+    const userById = await User.findOne({
+      attributes: { exclude: ['password'] },
+      where: { id }
+    });
+    if (!userById) {
+      return res.status(404).json({ message: 'No user found with this id' });
     }
-    catch (e) {
-        res.status(400).json({Error: e});
-      }
+    res.status(200).json({Updated: userById});
+  }
+  catch (e) {
+    res.status(400).json({ Error: e });
+  }
 });
 
-// PUT /api/users/1
-router.put('/:id', (req, res) => {
+// DELETE /api/users/id
+router.delete('/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const userById = await User.findOne({
+      attributes: { exclude: ['password'] },
+      where: { id }
+    });
+    if (!userById) {
+      return res.status(404).json({ message: 'No user found with this id' });
+    }
+    await User.destroy({ where: { id } });
+    res.status(200).json({ Deleted: userById });
 
-
-});
-
-// DELETE /api/users/1
-router.delete('/:id', (req, res) => {});
+  } catch (e) {
+    res.status(400).json(e);
+  }
+ });
 
 module.exports = router;
